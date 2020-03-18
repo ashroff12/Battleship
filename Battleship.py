71fec0e36ab4f824f211d251153e83c98feb815a
ashroff12 <51920473+ashroff12@users.noreply.github.com>
2020-03-17 19:25:19 -0500

    Add files via upload

--- Battleship.py
+++ Battleship.py
@@ -1,3 +1,14 @@
+from tkinter import *
+
+root = Tk()
+btn_player_grid = []
+btn_opponent_grid = []
+
+frm_player = Frame(root)
+frm_opponent = Frame(root)
+
+frm_ships = Frame(root)
+
 WIDTH = 10
 HEIGHT = 10
 P1_attack = []
@@ -29,7 +40,49 @@ def fill_board(board):
 def show_board(board):
     for i in range(HEIGHT):
         print(board[i])
-def place_ship(symbol, length, row, col, orient, board):
+
+def create_UI():
+    lb1 = Label(frm_player, text="My Ships", font="Arial 16 bold")
+    lb1.grid(row=0, column=0, columnspan=11)
+
+    for i in range(WIDTH):
+        lb = Label(frm_player, text=chr(i + 65))
+        lb.grid(row=1, column=i+1)
+    for i in range(HEIGHT):
+        lb = Label(frm_player, text = i + 1)
+        lb.grid(row=i+2, column= 0)
+
+        btn_player_grid.append([])
+        for j in range(WIDTH):
+            btn_player_grid[i].append(Button(frm_player, width=2, height=1))
+            btn_player_grid[i][j].row = i
+            btn_player_grid[i][j].col = j
+            btn_player_grid[i][j].bind("<Button-1>", place_ship)
+            btn_player_grid[i][j].grid(row=i+2, column=j+1)
+
+
+    lb1 = Label(frm_opponent, text="Opponent Ships", font="Arial 16 bold")
+    lb1.grid(row=0, column=0, columnspan=11)
+
+    for i in range(WIDTH):
+        lb = Label(frm_opponent, text=chr(i + 65))
+        lb.grid(row=1, column=i+1)
+    for i in range(HEIGHT):
+        lb = Label(frm_opponent, text = i + 1)
+        lb.grid(row=i+2, column= 0)
+
+        btn_opponent_grid.append([])
+        for j in range(WIDTH):
+            btn_opponent_grid[i].append(Button(frm_opponent, width=2, height=1))
+            btn_opponent_grid[i][j].row = i
+            btn_opponent_grid[i][j].col = j
+            btn_opponent_grid[i][j].bind("<Button-1>", place_ship)
+            btn_opponent_grid[i][j].grid(row=i+2, column=j+1)
+        
+    
+def place_ship(event):
+    print(event.widget.row, event.widget.col)
+'''
     if orient == "hor":
         if col + length > WIDTH:
             print("invaild ship")
@@ -44,6 +97,7 @@ def place_ship(symbol, length, row, col, orient, board):
         else:
             for i in range(length):
                 board[row + i][col] = symbol
+    '''
 def ship_placement(board):
     for i in range (len(P1_shiplist)):
         print("Place Ship " + P1_shiplist[i][0] + ", length" + str(P1_shiplist[i][1]))
@@ -64,7 +118,10 @@ def start_game():
     fill_board(P1_ships)
     fill_board(P2_ships)
 
-start_game()
-ship_placement(P1_attack)
-show_board(P1_attack)
+create_UI()
 
+frm_player.grid(row = 0, column = 0)
+frm_opponent.grid(row=0, column = 1)
+frm_ships.grid(row=1, column = 0, columnspan=2)
+
+root.mainloop
